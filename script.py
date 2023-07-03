f6d4a342a3da91e4fd11ddca5c1027ceed16b7b7
Vellforzi <vellforzi@gmail.com>
2023-07-03 20:10:03 +0300

    Import all requirements (module) and data.

--- script.py
+++ script.py
@@ -4,4 +4,24 @@ import seaborn as sns
 
 # load data
 df = pd.read_csv("all_data.csv")
-df.head()
+# print(df.head())
+# print(df.shape)
+print(f'-' * 200)
+
+# what countries do we have?
+print(df.Country.unique())
+print(f'-' * 200)
+
+# which years are represented in the data?
+print(df.Year.unique())
+print(f'-' * 200)
+
+# let's clean a column name
+df = df.rename({"Life expectancy at birth (years)": "LEABY"}, axis="columns")
+print(df.head())
+
+
+plt.figure(figsize=(8, 6))
+sns.histplot(df.GDP)
+plt.xlabel("GDP in Trillions of U.S. Dollars")
+plt.show()
